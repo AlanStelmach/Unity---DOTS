16279887c3c629f605bef0414bbfcaa8cf64a5a9
Alan Stelmach <alan.stelmach1@gmail.com>
2021-12-17 01:05:34 +0100

    Spawning multiple objects

--- Assets/Scripts/MonoBehaviours/SetupSpawner.cs
+++ Assets/Scripts/MonoBehaviours/SetupSpawner.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class SetupSpawner : MonoBehaviour
 {
@@ -7,6 +9,7 @@ public class SetupSpawner : MonoBehaviour
     [SerializeField] private int gridSize;
     [SerializeField] private int spread;
     private BlobAssetStore blob;
+    [SerializeField] private Vector2 speedRange = new Vector2(4, 7);
 
     private void Start()
     {
@@ -14,7 +17,18 @@ public class SetupSpawner : MonoBehaviour
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blob);
         var entity = GameObjectConversionUtility.ConvertGameObjectHierarchy(personPrefab, settings);
         var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        entityManager.Instantiate(entity);
+        for(int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                var instance = entityManager.Instantiate(entity);
+                float3 position = new float3(x * spread, 0, z * spread);
+                entityManager.SetComponentData(instance, new Translation {Value = position});
+                entityManager.SetComponentData(instance, new Destination {Value = position});
+                float speed = UnityEngine.Random.Range(speedRange.x, speedRange.y);
+                entityManager.SetComponentData(instance, new MovementSpeed {Speed = speed});
+            }
+        }
     }
 
     private void OnDestroy()
